219e601047e348bc3122272c9b34957e4db909a6
kgryte <kgryte@gmail.com>
2016-03-08 19:02:36 -0800

    Fix eqn in examples

--- examples/index.js
+++ examples/index.js
@@ -9,5 +9,5 @@ var i;
 for ( i = 0; i < 100; i++ ) {
 	b = Math.round( Math.random()*10 );
 	x = Math.round( Math.random()*10 ) - 5;
-	console.log( '%d^%d = %d', b, x, powm1( b, x ) );
+	console.log( '%d^%d - 1 = %d', b, x, powm1( b, x ) );
 }
